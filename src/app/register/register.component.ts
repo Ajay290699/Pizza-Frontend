c751822d311503b393d16efdb753c25a6f479067
Ajay Chavan <ajayc9838@gmail.com>
2023-01-19 12:07:11 +0530

    registration component done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private fb:FormBuilder){}
+
+  registerForm=this.fb.group({
+    firstName:[null,Validators.required],
+    lastName:[null,Validators.required],
+    email:[null,Validators.required],
+    password:[null,Validators.required,Validators.minLength(6)]
+  })
+
+  onSubmit(){
+    alert("Registered Successfully");
+  }
 
 }
